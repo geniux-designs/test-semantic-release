f03c71677c9fc086ff70c2f3e38b66445a2ef244
Haukur Mar Bodvarsson <haukurmar@gmail.com>
2024-11-13 21:23:20 +0100

    feat: added message to release git

--- release.config.mjs
+++ release.config.mjs
@@ -17,7 +17,8 @@ export default {
         [
             "@semantic-release/git",
             {
-                "assets": ["./CHANGELOG.md"]
+                "assets": ["./CHANGELOG.md", "./package.json"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
         ],
     ]
